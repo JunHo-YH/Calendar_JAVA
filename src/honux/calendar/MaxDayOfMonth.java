5b96e72dbeffc776c9ca718f1aad75c0a259f5a2
JunHo KANG <66174425+JunHo-YH@users.noreply.github.com>
2020-11-29 23:49:32 +0900

    Fix alorithm

--- src/honux/calendar/MaxDayOfMonth.java
+++ src/honux/calendar/MaxDayOfMonth.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class MaxDayOfMonth {
 	public static int getMaxDay(int month) {
 		int[] Month = {31, 28, 30, 31, 30, 31, 30, 31, 30, 31, 30, 31};
-		return Month[month];
+		return Month[month-1];
 	}
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("달을 입력하세요");
+		System.out.print("달을 입력하세요: ");
 		int Month = scan.nextInt();
 		
 		System.out.println(getMaxDay(Month));
