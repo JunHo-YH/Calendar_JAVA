75da3e30895d8c7a2a835392e31224588f06dd35
JunHo KANG <66174425+JunHo-YH@users.noreply.github.com>
2020-12-02 21:57:40 +0900

    Add function of LeapYear

--- src/honux/calendar/MaxDayOfMonth.java
+++ src/honux/calendar/MaxDayOfMonth.java
@@ -3,28 +3,48 @@ package honux.calendar;
 import java.util.Scanner;
 
 public class MaxDayOfMonth {
-	public static int getMaxDay(int month) {
-		int[] month_day = { 31, 28, 30, 31, 30, 31, 30, 31, 30, 31, 30, 31 };
-		return month_day[month - 1];
+	private static final int[] MAX_DAYS = { 31, 28, 30, 31, 30, 31, 30, 31, 30, 31, 30, 31 };
+	private static final int[] Leap_MAX_DAYS = { 31, 29, 30, 31, 30, 31, 30, 31, 30, 31, 30, 31 };
+
+	public static boolean isLeapYear(int year) { // 윤년 확인하는 메서드
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.print("달을 입력하세요: ");
-		int month = scan.nextInt();
-		
-		System.out.printf("   << %d년 %d월 >>\n", 2020, month);
-		for(int i = 1; i <= getMaxDay(month); i++) {
-			
-			if(i > 0 && i < 10) {
-				System.out.print(" " + i + " ");	
-			}else {
-				System.out.print(""+ i + " ");
+	public static int getMaxDay(int year, int month) { // 해당 월의 마지막 일을 출력하는 메서드
+		if (isLeapYear(year)) {
+			return Leap_MAX_DAYS[month - 1];
+		} else {
+			return MAX_DAYS[month - 1];
+		}
+	}
+
+	public static void printCalendar(int year, int month) {
+		System.out.printf("   << %d년 %d월 >>\n", year, month);
+		for (int i = 1; i <= getMaxDay(year, month); i++) {
+
+			if (i > 0 && i < 10) {
+				System.out.print(" " + i + " ");
+			} else {
+				System.out.print(i + " ");
 			}
-			if(i % 7 == 0) {
+			if (i % 7 == 0) {
 				System.out.println();
 			}
 		}
 	}
 
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("년도를 입력하세요 : ");
+		int year = scan.nextInt();
+		System.out.print("달을 입력하세요 : ");
+		int month = scan.nextInt();
+
+		printCalendar(year, month);
+
+	}
 }
